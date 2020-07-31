49b9789b82cc5e6a3da24ef30d7f304bb910a84d
Jack Shi <jshi@ivent.co.nz>
2020-07-31 16:57:14 +1200

    add stairs issue

--- recursion/recursion/recursion.c
+++ recursion/recursion/recursion.c
@@ -149,3 +149,101 @@ class Solution:
 					a = Row_Pre[x] + Row_Pre[x - 1]
 					Row_List.append(a)
 					return Row_List
+
+
+/*
+ 斐波那契数
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), for N > 1.
+Given N, calculate F(N).
+
+
+
+Example 1:
+
+Input: 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+
+这里面我们用了一个表去存已经递归过的元素，
+因为在递归过程中会产生很多的已经递归过的变量重复出现，所以我们利用这个表存储中间的变量
+计算机在处理过程中发现如何这个变量在之前已经分析过则直接返回元素。
+*/
+
+int Temp[31] = { 0 }; 
+int fib(int N) {
+	if (N == 0)
+	{
+		Temp[0] = 0;
+		return 0;
+	}
+	if (N == 1)
+	{
+		Temp[1] = 1;
+		return 1;
+	}
+	if (Temp[N] != 0) return Temp[N];     //在这里面我们直接返回了已经分析过的元素。
+
+	Temp[N] = fib(N - 1) + fib(N - 2);
+
+	return Temp[N];
+
+}
+/*
+爬楼梯
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+这个题目的思想和之前的是一样的我们发现爬楼梯 N的次数相当于前面 N-1和N-2 次数的和
+
+首先我们找到限制边界 楼层是1 和 2 然后我们用表去记录每次的数据
+这道题和费波那奇数列一个道理 请记住这个模板
+*/
+
+
+int temp[46] = { 0 };
+int climbStairs(int n) {
+	if (n == 1)
+	{
+		temp[n] = 1;
+		return 1;
+	}
+	if (n == 2) {
+		temp[n] = 2;
+		return 2;
+	}
+	if (temp[n] != 0) return temp[n];
+	temp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+
+	return temp[n];
+}
